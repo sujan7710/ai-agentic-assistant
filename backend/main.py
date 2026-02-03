c109c16d9d5c8f575635de96466ef8b2b036ae7c
sujan7710 <sujanvsm@gmail.com>
2026-02-04 01:42:24 +0530

    fix backend cors

--- backend/main.py
+++ backend/main.py
@@ -40,7 +40,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://localhost:5174"
+        "https://ai-agentic-frontend.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
